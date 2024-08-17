73955ff117f4b5b3d93b40de8c0bd819d8bbbf3c
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 06:00:48 +0100

    Update shopItems.js

--- shop/shopItems.js
+++ shop/shopItems.js
@@ -1,10 +1,10 @@
 // shopItems.js
 
 const shopItems = [
-    { id: 'https://sxdgoth.github.io/mwahbaby/assets/shirts/avatar-jacket.svg', name: 'Jacket', type: 'jacket', price: 100 },
-    { id: 'https://sxdgoth.github.io/mwahbaby/assets/shirts/avatar-hoodie.svg', name: 'Hoodie', type: 'jacket', price: 150 },
-    { id: 'https://sxdgoth.github.io/mwahbaby/assets/shirts/avatar-body.svg', name: 'Body', type: 'body', price: 150 },
-    { id: 'https://sxdgoth.github.io/mwahbaby/assets/shirts/avatar-head.svg', name: 'Head', type: 'head', price: 150 },
-    { id: 'https://sxdgoth.github.io/mwahbaby/assets/shirts/mostvaluableshirt.svg', name: 'Most Valuable Shirt', type: 'shirt', price: 200 },
-    { id: 'https://sxdgoth.github.io/mwahbaby/assets/shirts/najabloomtop.svg', name: 'Naja Bloom Top', type: 'top', price: 180 },
+    { id: 'avatar-jacket.svg', name: 'Jacket', type: 'jacket', price: 100 },
+    { id: 'avatar-hoodie.svg', name: 'Hoodie', type: 'jacket', price: 150 },
+    { id: 'avatar-body.svg', name: 'Body', type: 'body', price: 150 },
+    { id: 'avatar-head.svg', name: 'Head', type: 'head', price: 150 },
+    { id: 'mostvaluableshirt.svg', name: 'Most Valuable Shirt', type: 'shirt', price: 200 },
+    { id: 'najabloomtop.svg', name: 'Naja Bloom Top', type: 'top', price: 180 },
 ];
