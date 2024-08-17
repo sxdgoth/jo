b88bc632145687ea21e45e5777c8536fbbbc14b9
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 17:47:12 +0100

    Update login.js

--- login.js
+++ login.js
@@ -10,28 +10,17 @@ function login() {
 
     if (user) {
         console.log('User found:', user);
-        
-        // Load user-specific data
-        const storedUser = JSON.parse(localStorage.getItem(username));
-        if (storedUser) {
-            // Use the stored data, including inventory
-            user.coins = storedUser.coins;
-            user.inventory = storedUser.inventory || [];
-        } else {
-            // If no stored data, initialize with default values
-            user.coins = user.coins || 1000; // Default to 1000 if not set
-            user.inventory = [];
-            localStorage.setItem(username, JSON.stringify(user));
-        }
-
-        // Update the logged in user in localStorage
-        localStorage.setItem('loggedInUser', JSON.stringify(user));
-        
+        sessionStorage.setItem('loggedInUser', JSON.stringify(user));
         window.location.href = 'home/index.html';
     } else {
         console.log('Invalid login attempt');
         alert('Invalid username or password.');
     }
+}
+
+function clearLoginForm() {
+    document.getElementById('login-username').value = '';
+    document.getElementById('login-password').value = '';
 }
 
 
