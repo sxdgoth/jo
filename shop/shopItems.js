0fc5f9f119aee3ab89a8591ddc807b168e9afc27
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 06:01:28 +0100

    Update shopItems.js

--- shop/shopItems.js
+++ shop/shopItems.js
@@ -1,10 +1,10 @@
 // shopItems.js
 
 const shopItems = [
-    { id: 'avatar-jacket.svg', name: 'Jacket', type: 'jacket', price: 100 },
-    { id: 'avatar-hoodie.svg', name: 'Hoodie', type: 'jacket', price: 150 },
-    { id: 'avatar-body.svg', name: 'Body', type: 'body', price: 150 },
-    { id: 'avatar-head.svg', name: 'Head', type: 'head', price: 150 },
-    { id: 'mostvaluableshirt.svg', name: 'Most Valuable Shirt', type: 'shirt', price: 200 },
-    { id: 'najabloomtop.svg', name: 'Naja Bloom Top', type: 'top', price: 180 },
+    { id: 'avatar-jacket.svg', name: 'Jacket', type: 'jacket', price: 100, path: 'home/assets/body/' },
+    { id: 'avatar-hoodie.svg', name: 'Hoodie', type: 'jacket', price: 150, path: 'home/assets/body/' },
+    { id: 'avatar-body.svg', name: 'Body', type: 'body', price: 150, path: 'home/assets/body/' },
+    { id: 'avatar-head.svg', name: 'Head', type: 'head', price: 150, path: 'home/assets/body/' },
+    { id: 'mostvaluableshirt.svg', name: 'Most Valuable Shirt', type: 'shirt', price: 200, path: 'home/assets/shirts/' },
+    { id: 'najabloomtop.svg', name: 'Naja Bloom Top', type: 'top', price: 180, path: 'home/assets/shirts/' },
 ];
