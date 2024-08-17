6f70d3d056c72426a48aba0089a25f2f0cc7648c
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 06:21:36 +0100

    Update avatarTemplate.js

--- shop/avatarTemplate.js
+++ shop/avatarTemplate.js
@@ -5,10 +5,10 @@ class AvatarBody {
         this.container = document.getElementById(containerId);
         this.baseUrl = 'https://sxdgoth.github.io/jo/home/assets/body/';
         this.bodyParts = [
-            { name: 'Legs', file: 'avatar-legsandfeet.svg' },
-            { name: 'Arms', file: 'avatar-armsandhands.svg' },
-            { name: 'Body', file: 'avatar-body.svg' },
-            { name: 'Head', file: 'avatar-head.svg' }
+            { name: 'Legs', file: 'avatar-legsandfeet.svg', type: 'Legs' },
+            { name: 'Arms', file: 'avatar-armsandhands.svg', type: 'Arms' },
+            { name: 'Body', file: 'avatar-body.svg', type: 'Body' },
+            { name: 'Head', file: 'avatar-head.svg', type: 'Head' }
         ];
     }
 
@@ -19,6 +19,7 @@ class AvatarBody {
             const img = document.createElement('img');
             img.src = this.baseUrl + part.file;
             img.alt = part.name;
+            img.dataset.type = part.type;
             img.style.position = 'absolute';
             img.style.top = '0';
             img.style.left = '0';
@@ -28,6 +29,7 @@ class AvatarBody {
             img.onerror = () => console.error(`Failed to load ${part.name}: ${img.src}`);
             this.container.appendChild(img);
         });
+        layerManager.reorderLayers();
     }
 }
 
@@ -36,3 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const avatarBody = new AvatarBody('avatar-display');
     avatarBody.loadAvatar();
 });
+
+
+
+
