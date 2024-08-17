0ba32f497e176bcc72dad0d353aab669fbf00247
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 08:20:59 +0100

    Update shopItems.js

--- shop/shopItems.js
+++ shop/shopItems.js
@@ -5,6 +5,6 @@ const shopItems = [
     { id: 'avatar-hoodie.svg', name: 'Hoodie', type: 'Jacket', price: 150, path: 'home/assets/shirts/' },
     { id: 'avatar-body.svg', name: 'Body', type: 'Body', price: 150, path: 'home/assets/shirts/' },
     { id: 'avatar-head.svg', name: 'Head', type: 'Head', price: 150, path: 'home/assets/shirts/' },
-    { id: 'mostvaluableshirt.svg', name: 'Most Valuable Shirt', type: 'Body', price: 200, path: 'home/assets/shirts/' },
+    { id: 'mostvaluableshirt.svg', name: 'Most Valuable Shirt', type: 'Shirt', price: 200, path: 'home/assets/shirts/' },
     { id: 'najabloomtop.svg', name: 'Naja Bloom Top', type: 'Shirt', price: 180, path: 'home/assets/shirts/' },
 ];
