96cbb44e61cbe5db9b94a683ba018e4912acd6e0
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 05:13:11 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,19 @@
 function login() {
+    console.log('Login function called');
+
     const username = document.getElementById('login-username').value;
     const password = document.getElementById('login-password').value;
+    console.log('Username:', username, 'Password:', password);
 
     const users = JSON.parse(localStorage.getItem('users')) || [];
     const user = users.find(u => u.username === username && u.password === password);
 
     if (user) {
+        console.log('User found:', user);
         sessionStorage.setItem('loggedInUser', JSON.stringify(user));
         window.location.href = 'shop/index.html';
     } else {
+        console.log('Invalid login attempt');
         alert('Invalid username or password.');
     }
 }
