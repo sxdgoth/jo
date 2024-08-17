d3be6dedfb5442fd6786637b8aab69f541a55673
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 21:04:38 +0100

    Update wardrobe.js

--- wardrobe/wardrobe.js
+++ wardrobe/wardrobe.js
@@ -39,6 +39,8 @@ function renderOwnedItems() {
         const itemImage = itemElement.querySelector('.item-image');
         itemImage.addEventListener('click', () => toggleItem(item));
     });
+
+    window.avatarManager.updateItemVisuals();
 }
 
 function toggleItem(item) {
