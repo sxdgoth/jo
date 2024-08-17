4a3dbdb0112fcc9398899e4bdb6c2f5f511f45c0
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 05:24:09 +0100

    Update avatarManager.js

--- shop/avatarManager.js
+++ shop/avatarManager.js
@@ -2,34 +2,6 @@
 
 console.log("avatarManager.js loaded");
 
-class AvatarBody {
-    constructor(containerId) {
-        this.container = document.getElementById(containerId);
-        this.baseUrl = 'https://sxdgoth.github.io/mwahbaby/assets/legendary/';
-        this.bodyParts = [
-            { name: 'Legs', file: 'avatar-legsandfeet.svg' },
-            { name: 'Arms', file: 'avatar-armsandhands.svg' },
-            { name: 'Body', file: 'avatar-body.svg' },
-            { name: 'Head', file: 'avatar-head.svg' }
-        ];
-    }
-
-    loadAvatar() {
-        this.container.innerHTML = '';
-        this.bodyParts.forEach(part => {
-            const img = document.createElement('img');
-            img.src = this.baseUrl + part.file;
-            img.alt = part.name;
-            img.style.position = 'absolute';
-            img.style.top = '0';
-            img.style.left = '0';
-            img.style.width = '100%';
-            img.style.height = '100%';
-            this.container.appendChild(img);
-        });
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     console.log("DOM fully loaded");
     const avatarBody = new AvatarBody('avatar-display');
