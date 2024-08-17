97f2a6b8e97e91dd62bdcd14150f7bc9efae01cc
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 05:34:45 +0100

    Update avatarTemplate.js

--- shop/avatarTemplate.js
+++ shop/avatarTemplate.js
@@ -3,7 +3,7 @@
 class AvatarBody {
     constructor(containerId) {
         this.container = document.getElementById(containerId);
-        this.baseUrl = 'https://sxdgoth.github.io/jo/assets/body/';
+        this.baseUrl = 'https://sxdgoth.github.io/jo/home/assets/body/';
         this.bodyParts = [
             { name: 'Legs', file: 'avatar-legsandfeet.svg' },
             { name: 'Arms', file: 'avatar-armsandhands.svg' },
