22e7851e8a4a5de85dea1dc0bb5ef7abf2680c1f
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 04:35:03 +0100

    Update register.js

--- register.js
+++ register.js
@@ -1,12 +1,11 @@
 function register() {
     const username = document.getElementById('reg-username').value;
-    const email = document.getElementById('reg-email').value;
     const password = document.getElementById('reg-password').value;
 
-    if (username && email && password) {
+    if (username && password) {
         // In a real application, you would send this data to a server
         // For this example, we'll just store it in localStorage
-        localStorage.setItem('user', JSON.stringify({ username, email, password }));
+        localStorage.setItem('user', JSON.stringify({ username, password }));
         alert('Registration successful! You can now log in.');
         clearRegisterForm();
     } else {
@@ -16,6 +15,5 @@ function register() {
 
 function clearRegisterForm() {
     document.getElementById('reg-username').value = '';
-    document.getElementById('reg-email').value = '';
     document.getElementById('reg-password').value = '';
 }
