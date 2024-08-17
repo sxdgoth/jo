c780ec9272c20a145bbea50123e4233bdd865cf9
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-17 04:42:01 +0100

    Update register.js

--- register.js
+++ register.js
@@ -3,16 +3,11 @@ function register() {
     const password = document.getElementById('reg-password').value;
 
     if (username && password) {
-        const newUser = { username, password, coins: 1000 };
+        const newUser = { username, password, coins: 1000 }; // Here we set 1000 coins
         localStorage.setItem('user', JSON.stringify(newUser));
         alert('Registration successful! You can now log in.');
         clearRegisterForm();
     } else {
         alert('Please fill in all fields.');
     }
-}
-
-function clearRegisterForm() {
-    document.getElementById('reg-username').value = '';
-    document.getElementById('reg-password').value = '';
 }
