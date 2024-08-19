fc42d8099ce3708fa497bf9e5474ad84b844bd84
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-19 06:20:47 +0100

    Update shopItems.js

--- shop/shopItems.js
+++ shop/shopItems.js
@@ -2,10 +2,14 @@
 
 const shopItems = [
     { id: 'avatar-jacket.svg', name: 'Jacket', type: 'Jacket', price: 100, path: 'home/assets/shirts/' },
-    { id: 'avatar-hoodie.svg', name: 'Hoodie', type: 'Jacket', price: 150, path: 'home/assets/shirts/' },
-    { id: 'avatar-body.svg', name: 'Body', type: 'Body', price: 150, path: 'home/assets/shirts/' },
-    { id: 'avatar-head.svg', name: 'Head', type: 'Head', price: 150, path: 'home/assets/shirts/' },
     { id: 'mostvaluableshirt.svg', name: 'Most Valuable Shirt', type: 'Shirt', price: 200, path: 'home/assets/shirts/' },
-    { id: 'najabloomtop.svg', name: 'Naja Bloom Top', type: 'Shirt', price: 180, path: 'home/assets/shirts/' },
+    { id: 'svgexport-23.svg', name: 'Hemlock Snakeflame Shorts', type: 'Pants', price: 180, path: 'home/assets/pants/' },
+    { id: 'svgexport-24.svg', name: 'Hemlock Snakeflame Shorts', type: 'Pants', price: 180, path: 'home/assets/pants/' },
+    { id: 'svgexport-27.svg', name: 'Arsenic Snakeflame Shorts', type: 'Pants', price: 180, path: 'home/assets/pants/' },
+    { id: 'svgexport-25.svg', name: 'Hemlock Overgrowth Jeans', type: 'Pants', price: 180, path: 'home/assets/pants/' },
+    { id: 'svgexport-26.svg', name: 'Rotwood Overgrowth Jeans', type: 'Pants', price: 180, path: 'home/assets/pants/' },
+    { id: 'svgexport-34.svg', name: 'Charcoal Melody Pants', type: 'Pants', price: 180, path: 'home/assets/pants/' },
+    { id: 'svgexport-30.svg', name: 'Casual Creations Jeans', type: 'Pants', price: 180, path: 'home/assets/pants/' },
+    { id: 'ScarEye.svg', name: 'Scar Eyes', type: 'Eyes', price: 180, path: 'home/assets/eyes/' },
     // Add more items as needed
 ];
