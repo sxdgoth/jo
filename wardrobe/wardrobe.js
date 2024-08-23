168a178a622b4951e1c38932fffdc29c59c28efc
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-23 04:53:55 +0100

    Update wardrobe.js

--- wardrobe/wardrobe.js
+++ wardrobe/wardrobe.js
@@ -1,3 +1,5 @@
+// wardrobe.js
+
 document.addEventListener('DOMContentLoaded', function() {
     const loggedInUser = JSON.parse(sessionStorage.getItem('loggedInUser'));
     
@@ -47,15 +49,21 @@ function renderOwnedItems() {
             <p>Type: ${item.type}</p>
         `;
         wardrobeItemsContainer.appendChild(itemElement);
-        
         // Add click event listener to the item image
         const itemImage = itemElement.querySelector('.item-image');
-        itemImage.addEventListener('click', () => {
-            if (window.avatarManager) {
-                window.avatarManager.toggleItem(item);
-            } else {
-                console.error('AvatarManager not initialized');
-            }
-        });
+        itemImage.addEventListener('click', () => toggleItem(item));
     });
+}
+
+function toggleItem(item) {
+    if (window.avatarManager) {
+        window.avatarManager.toggleItem(item);
+    } else {
+        console.error('AvatarManager not initialized');
+    }
+}
+
+function logout() {
+    sessionStorage.removeItem('loggedInUser');
+    window.location.href = '../index.html';
 }
