44da92e7832020d78fd72bd0e9d38686498e4f10
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-23 17:26:26 +0100

    Update wardrobe.js

--- wardrobe/wardrobe.js
+++ wardrobe/wardrobe.js
@@ -49,11 +49,16 @@ function renderOwnedItems() {
             <p>Type: ${item.type}</p>
         `;
         wardrobeItemsContainer.appendChild(itemElement);
-        // Add click event listener to the item image
+        
+        // Apply positioning to the item image
         const itemImage = itemElement.querySelector('.item-image');
+        if (window.avatarManager) {
+            window.avatarManager.applyItemPosition(itemImage, item.type);
+        }
+        
+        // Add click event listener to the item image
         itemImage.addEventListener('click', () => toggleItem(item));
     });
-}
 
 function toggleItem(item) {
     if (window.avatarManager) {
