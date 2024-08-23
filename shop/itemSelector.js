8427c7f9170cb8b53f9925bac893f6766adee3bc
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-23 21:07:07 +0100

    Update itemSelector.js

--- shop/itemSelector.js
+++ shop/itemSelector.js
@@ -1,57 +1,63 @@
+// itemSelector.js
+
 class ItemSelector {
     constructor(avatarDisplay) {
-        console.log('ItemSelector: Initializing with avatarDisplay');
         this.avatarDisplay = avatarDisplay;
         this.selectedItems = {};
     }
 
-   toggleItem(itemId) {
-    console.log('ItemSelector: toggleItem called with itemId:', itemId);
-    const item = shopItems.find(i => i.id === itemId);
-    if (!item) {
-        console.error('ItemSelector: Item not found:', itemId);
-        return;
+    toggleItem(itemId) {
+        console.log('ItemSelector: toggleItem called with itemId:', itemId);
+        const item = shopItems.find(i => i.id === itemId);
+        if (!item) {
+            console.error('ItemSelector: Item not found:', itemId);
+            return;
+        }
+
+        if (this.selectedItems[item.type] === itemId) {
+            console.log('ItemSelector: Deselecting item');
+            this.deselectItem(item.type);
+        } else {
+            console.log('ItemSelector: Selecting item');
+            this.selectItem(item);
+        }
+
+        this.updateShopDisplay();
     }
-
-    if (this.selectedItems[item.type] === itemId) {
-        console.log('ItemSelector: Deselecting item');
-        this.deselectItem(item.type);
-    } else {
-        console.log('ItemSelector: Selecting item');
-        this.selectItem(item);
-    }
-
-    this.updateShopDisplay();
-}
-
 
     selectItem(item) {
-        console.log(`ItemSelector: Selecting item: ${item.name} (ID: ${item.id}, Type: ${item.type})`);
-        if (this.selectedItems[item.type]) {
-            this.avatarDisplay.removeItem(item.type);
-        }
         this.selectedItems[item.type] = item.id;
         this.avatarDisplay.tryOnItem(item);
     }
 
     deselectItem(type) {
-        console.log(`ItemSelector: Deselecting item of type: ${type}`);
         delete this.selectedItems[type];
         this.avatarDisplay.removeItem(type);
     }
 
     updateShopDisplay() {
-        if (window.shopManager) {
-            window.shopManager.renderShopItems();
-        }
+        document.querySelectorAll('.item-image').forEach(itemElement => {
+            const itemId = itemElement.dataset.id;
+            const item = shopItems.find(i => i.id === itemId);
+            if (item) {
+                itemElement.classList.toggle('selected', this.selectedItems[item.type] === itemId);
+            }
+        });
     }
 
     resetSelection() {
-        console.log('Resetting all selections');
-        Object.keys(this.selectedItems).forEach(type => {
-            this.avatarDisplay.removeItem(type);
-        });
         this.selectedItems = {};
         this.updateShopDisplay();
     }
 }
+
+// Initialize ItemSelector when the DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Initializing ItemSelector');
+    if (window.avatarDisplay) {
+        window.itemSelector = new ItemSelector(window.avatarDisplay);
+        console.log('ItemSelector initialized');
+    } else {
+        console.error('avatarDisplay not found when creating ItemSelector');
+    }
+});
