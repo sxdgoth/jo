b21005443a423ac63c084b2af34298de11eaac7b
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-24 22:31:09 +0100

    Update displayAvatar.js

--- shop/displayAvatar.js
+++ shop/displayAvatar.js
@@ -367,13 +367,20 @@ blendColors(color1, color2, ratio) {
         localStorage.setItem(`hairColor_${this.username}`, newColor);
     }
 
-   tryOnItem(item) {
+  tryOnItem(item) {
     console.log(`Trying on ${item.name} (ID: ${item.id}, Type: ${item.type})`);
     
+    // List of items that should not be unselected
+    const nonUnselectableItems = ['Pants', 'Shirt', 'Hair', 'Eyes', 'Mouth', 'Nose', 'Eyebrows'];
+    
     if (this.currentItems[item.type] && this.currentItems[item.type].id === item.id) {
-        // If the item is already applied, remove it
-        console.log(`Removing item ${item.id} of type ${item.type}`);
-        this.removeItem(item.type);
+        // If the item is already applied and it's not in the non-unselectable list, remove it
+        if (!nonUnselectableItems.includes(item.type)) {
+            console.log(`Removing item ${item.id} of type ${item.type}`);
+            this.removeItem(item.type);
+        } else {
+            console.log(`Cannot unselect item of type ${item.type}`);
+        }
     } else {
         // Apply the new item
         console.log(`Applying new item ${item.id} of type ${item.type}`);
