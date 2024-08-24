8a157d4e724bca85d1eec57f8d751af1c15237a4
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-24 20:23:07 +0100

    Update shopAvatarDisplay.js

--- shop/shopAvatarDisplay.js
+++ shop/shopAvatarDisplay.js
@@ -4,14 +4,17 @@ class ShopAvatarDisplay extends AvatarDisplay {
     constructor(containerId, username) {
         super(containerId, username);
         this.triedOnItems = {};
+        this.requiredTypes = ['Shirt', 'Pants', 'Nose', 'Eyes', 'Eyebrows', 'Mouth'];
     }
 
     tryOnItem(item) {
         console.log(`Trying on ${item.name} (ID: ${item.id}, Type: ${item.type})`);
         
         if (this.triedOnItems[item.type] && this.triedOnItems[item.type].id === item.id) {
-            // If the same item is clicked again, remove it
-            this.removeTriedOnItem(item.type);
+            // If the same item is clicked again, only remove it if it's not a required type
+            if (!this.requiredTypes.includes(item.type)) {
+                this.removeTriedOnItem(item.type);
+            }
         } else {
             // Apply the new item
             this.triedOnItems[item.type] = item;
@@ -30,23 +33,32 @@ class ShopAvatarDisplay extends AvatarDisplay {
             if (equippedItem) {
                 this.updateAvatarDisplay(type, `${equippedItem.path}${equippedItem.id}`);
             }
-        } else {
-            // If no equipped item, hide the layer
+        } else if (!this.requiredTypes.includes(type)) {
+            // If no equipped item and not a required type, hide the layer
             this.updateAvatarDisplay(type, '');
         }
     }
 
     resetTriedOnItems() {
         Object.keys(this.triedOnItems).forEach(type => {
-            this.removeTriedOnItem(type);
+            if (!this.requiredTypes.includes(type)) {
+                this.removeTriedOnItem(type);
+            }
         });
-        this.triedOnItems = {};
     }
 
     updateAvatarDisplay(type, src) {
         super.updateAvatarDisplay(type, src);
         if (window.layerManager) {
             window.layerManager.reorderLayers();
+        }
+        this.applyItemPosition(type);
+    }
+
+    applyItemPosition(type) {
+        const layer = this.layers[type];
+        if (layer && window.applyItemPosition) {
+            window.applyItemPosition(layer, type);
         }
     }
 }
