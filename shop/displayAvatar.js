cbdd95a1a90cab6e58dd4b80c72262c04ee958c9
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-08-25 04:07:50 +0100

    Update displayAvatar.js

--- shop/displayAvatar.js
+++ shop/displayAvatar.js
@@ -167,7 +167,7 @@ class AvatarDisplay {
     }
 
     reorderLayers() {
-        const order = ['Legs', 'Arms', 'Body', 'Shoes', 'Pants', 'Dress', 'Shirt', 'Jacket', 'Backhair',  'Neck', 'Hoodie', 'Head', 'Cheeks', 'Eyes', 'Mouth', 'Nose', 'Face', 'Eyebrows', 'Accessories', 'Hair'];
+         const order = ['Legs', 'Arms', 'Body', 'Shoes', 'Pants', 'Dress', 'Shirt', 'Jacket', 'Hoodie', 'Backhair', 'Neck', 'Head', 'Cheeks', 'Eyes', 'Mouth', 'Nose', 'Face', 'Eyebrows', 'Accessories', 'Hair'];
         order.forEach((type, index) => {
             if (this.layers[type]) {
                 this.layers[type].style.zIndex = index + 1;
