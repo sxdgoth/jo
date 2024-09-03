3bbda3f1dd4057434cb66db92f61fc2b571127c3
sxdgoth <93499323+sxdgoth@users.noreply.github.com>
2024-09-03 02:50:29 +0100

    Update admin.js

--- control/admin.js
+++ control/admin.js
@@ -1,4 +1,4 @@
-const GITHUB_REPO = 'https://api.github.com/repos/sxdgoth/myadminhighrise/contents/users.json';
+const GITHUB_REPO = 'https://api.github.com/repos/sxdgoth/jo/contents/users.json';
 const GITHUB_TOKEN = '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';
 
 async function loadUsers() {
